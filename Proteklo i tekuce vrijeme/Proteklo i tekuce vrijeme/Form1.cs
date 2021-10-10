6610aaf4f193238a5f65a206e9e132b07c389e26
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-10-10 20:10:37 +0200

    Add files via upload

---
+++ Proteklo i tekuce vrijeme/Proteklo i tekuce vrijeme/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proteklo_i_tekuce_vrijeme
+{
+
+    public partial class Form1 : Form
+    {
+        DateTime pocetno;
+
+        public Form1(){
+
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime pocetno = DateTime.Now;
+            toolStripStatusLabel2.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = DateTime.Now - pocetno;
+            toolStripStatusLabel1.Text = "Proteklo: " +
+            proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
